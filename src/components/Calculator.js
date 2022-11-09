469745d28668b0b6322e4f236bdde9d09da8388d
rmrmrmr <ricardomartineza77@gmail.com>
2022-11-09 11:09:32 -0600

    Add events to calculator buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,29 +1,50 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
-class Calculator extends React.PureComponent {
+class Calculator extends React.Component {
+  constructor(buttonrobuttons) {
+    super(buttonrobuttons);
+    this.state = {
+      total: null,
+      next: null,
+      obuttoneration: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = (event) => {
+    const result = calculate(this.state, event.target.textContent);
+    this.setState(result);
+  }
+
   render() {
+    const { total, next, operation } = this.state;
     return (
       <div className="flex flex-wrap w-screen h-screen">
-        <p className="flex w-full h-1/6 bg-slate-500 text-white text-8xl items-center justify-end px-3">0</p>
-        <p className="greyBttn">AC</p>
-        <p className="greyBttn">+/-</p>
-        <p className="greyBttn">%</p>
-        <p className="orangeBttn">÷</p>
-        <p className="greyBttn">7</p>
-        <p className="greyBttn">8</p>
-        <p className="greyBttn">9</p>
-        <p className="orangeBttn">x</p>
-        <p className="greyBttn">4</p>
-        <p className="greyBttn">5</p>
-        <p className="greyBttn">6</p>
-        <p className="orangeBttn">-</p>
-        <p className="greyBttn">1</p>
-        <p className="greyBttn">2</p>
-        <p className="greyBttn">3</p>
-        <p className="orangeBttn">+</p>
-        <p className="greyBttn w-1/2">0</p>
-        <p className="greyBttn">.</p>
-        <p className="orangeBttn">=</p>
+        <span className="flex w-full h-1/6 bg-slate-500 text-white text-8xl items-center justify-end buttonx-3">
+          { total }
+          { operation }
+          { next }
+        </span>
+        <button className="greyBttn" type="button" name="AC" onClick={this.handleClick}>AC</button>
+        <button className="greyBttn" type="button" name="+/-" onClick={this.handleClick}>+/-</button>
+        <button className="greyBttn" type="button" name="%" onClick={this.handleClick}>%</button>
+        <button className="orangeBttn" type="button" name="÷" onClick={this.handleClick}>÷</button>
+        <button className="greyBttn" type="button" name="7" onClick={this.handleClick}>7</button>
+        <button className="greyBttn" type="button" name="8" onClick={this.handleClick}>8</button>
+        <button className="greyBttn" type="button" name="9" onClick={this.handleClick}>9</button>
+        <button className="orangeBttn" type="button" name="x" onClick={this.handleClick}>x</button>
+        <button className="greyBttn" type="button" name="4" onClick={this.handleClick}>4</button>
+        <button className="greyBttn" type="button" name="5" onClick={this.handleClick}>5</button>
+        <button className="greyBttn" type="button" name="6" onClick={this.handleClick}>6</button>
+        <button className="orangeBttn" type="button" name="-" onClick={this.handleClick}>-</button>
+        <button className="greyBttn" type="button" name="1" onClick={this.handleClick}>1</button>
+        <button className="greyBttn" type="button" name="3" onClick={this.handleClick}>2</button>
+        <button className="greyBttn" type="button" name="3" onClick={this.handleClick}>3</button>
+        <button className="orangeBttn" type="button" name="+" onClick={this.handleClick}>+</button>
+        <button className="greyBttn w-1/2" type="button" name="0" onClick={this.handleClick}>0</button>
+        <button className="greyBttn" type="button" name="." onClick={this.handleClick}>.</button>
+        <button className="orangeBttn" type="button" name="=" onClick={this.handleClick}>=</button>
       </div>
     );
   }
