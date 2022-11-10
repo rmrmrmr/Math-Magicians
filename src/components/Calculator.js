d3ec7c8a6168d1cc3616cb3e463b6be65dde1993
rmrmrmr <ricardomartineza77@gmail.com>
2022-11-10 11:23:46 -0600

    Refactor with React Hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,53 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(buttonrobuttons) {
-    super(buttonrobuttons);
-    this.state = {
-      total: null,
-      next: null,
-      obuttoneration: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  handleClick = (event) => {
-    const result = calculate(this.state, event.target.textContent);
-    this.setState(result);
-  }
+  const handleClick = (event) => {
+    const { name } = event.target;
+    const newState = calculate(state, name);
+    setState(newState);
+  };
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="flex flex-wrap w-screen h-screen">
-        <span className="flex w-full h-1/6 bg-slate-500 text-white text-8xl items-center justify-end buttonx-3">
-          { total }
-          { operation }
-          { next }
-        </span>
-        <button className="greyBttn" type="button" name="AC" onClick={this.handleClick}>AC</button>
-        <button className="greyBttn" type="button" name="+/-" onClick={this.handleClick}>+/-</button>
-        <button className="greyBttn" type="button" name="%" onClick={this.handleClick}>%</button>
-        <button className="orangeBttn" type="button" name="÷" onClick={this.handleClick}>÷</button>
-        <button className="greyBttn" type="button" name="7" onClick={this.handleClick}>7</button>
-        <button className="greyBttn" type="button" name="8" onClick={this.handleClick}>8</button>
-        <button className="greyBttn" type="button" name="9" onClick={this.handleClick}>9</button>
-        <button className="orangeBttn" type="button" name="x" onClick={this.handleClick}>x</button>
-        <button className="greyBttn" type="button" name="4" onClick={this.handleClick}>4</button>
-        <button className="greyBttn" type="button" name="5" onClick={this.handleClick}>5</button>
-        <button className="greyBttn" type="button" name="6" onClick={this.handleClick}>6</button>
-        <button className="orangeBttn" type="button" name="-" onClick={this.handleClick}>-</button>
-        <button className="greyBttn" type="button" name="1" onClick={this.handleClick}>1</button>
-        <button className="greyBttn" type="button" name="3" onClick={this.handleClick}>2</button>
-        <button className="greyBttn" type="button" name="3" onClick={this.handleClick}>3</button>
-        <button className="orangeBttn" type="button" name="+" onClick={this.handleClick}>+</button>
-        <button className="greyBttn w-1/2" type="button" name="0" onClick={this.handleClick}>0</button>
-        <button className="greyBttn" type="button" name="." onClick={this.handleClick}>.</button>
-        <button className="orangeBttn" type="button" name="=" onClick={this.handleClick}>=</button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="flex flex-wrap w-screen h-screen">
+      <span className="flex w-full h-1/6 bg-slate-500 text-white text-8xl items-center justify-end buttonx-3">
+        { state.total }
+        { state.operation }
+        { state.next }
+      </span>
+      <button className="greyBttn" type="button" name="AC" onClick={handleClick}>AC</button>
+      <button className="greyBttn" type="button" name="+/-" onClick={handleClick}>+/-</button>
+      <button className="greyBttn" type="button" name="%" onClick={handleClick}>%</button>
+      <button className="orangeBttn" type="button" name="÷" onClick={handleClick}>÷</button>
+      <button className="greyBttn" type="button" name="7" onClick={handleClick}>7</button>
+      <button className="greyBttn" type="button" name="8" onClick={handleClick}>8</button>
+      <button className="greyBttn" type="button" name="9" onClick={handleClick}>9</button>
+      <button className="orangeBttn" type="button" name="x" onClick={handleClick}>x</button>
+      <button className="greyBttn" type="button" name="4" onClick={handleClick}>4</button>
+      <button className="greyBttn" type="button" name="5" onClick={handleClick}>5</button>
+      <button className="greyBttn" type="button" name="6" onClick={handleClick}>6</button>
+      <button className="orangeBttn" type="button" name="-" onClick={handleClick}>-</button>
+      <button className="greyBttn" type="button" name="1" onClick={handleClick}>1</button>
+      <button className="greyBttn" type="button" name="3" onClick={handleClick}>2</button>
+      <button className="greyBttn" type="button" name="3" onClick={handleClick}>3</button>
+      <button className="orangeBttn" type="button" name="+" onClick={handleClick}>+</button>
+      <button className="greyBttn w-1/2" type="button" name="0" onClick={handleClick}>0</button>
+      <button className="greyBttn" type="button" name="." onClick={handleClick}>.</button>
+      <button className="orangeBttn" type="button" name="=" onClick={handleClick}>=</button>
+    </div>
+  );
+};
 
 export default Calculator;
