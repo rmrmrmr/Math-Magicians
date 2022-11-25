f0b6835a964c91704548fa91566921c949663851
rmrmrmr <ricardomartineza77@gmail.com>
2022-11-25 15:22:02 -0600

    Update Home.js quote

--- src/components/Home.js
+++ src/components/Home.js
@@ -42,19 +42,6 @@ export default function Home() {
         in five groups. Each of these groups expresses, by itself, certain related fundamental
         facts of our intuition. We will name these groups as follows:
         <br />
-        &emsp; Let us consider three distinct systems of things. The things composing the first
-        system, we will call points and designate them by the letters A, B, C,. . . ; those of
-        the second, we will call straight lines and designate them by the letters a, b, c,. . . ;
-        and those of the third system, we will call planes and designate them by the Greek
-        letters α, β, γ,. . . The points are called the elements of linear geometry; the points
-        and straight lines, the elements of plane geometry; and the points, lines, and planes,
-        the elements of the geometry of space or the elements of space. We think of these
-        points, straight lines, and planes as having certain mutual relations, which we indicate
-        by means of such words as “are situated,” “between,” “parallel,” “congruent,” “continuous,”
-        etc. The complete and exact description of these relations follows as a consequence of
-        the axioms of geometry. These axioms may be arranged
-        in five groups. Each of these groups expresses, by itself, certain related fundamental
-        facts of our intuition. We will name these groups as follows:
         <br />
         (from
         <em> The Foundations of geometry </em>
