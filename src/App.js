4454d9e257b019cbbfd528458952c41a1aa624cd
rmrmrmr <ricardomartineza77@gmail.com>
2022-11-25 14:42:45 -0600

    Add React routing and React basic styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  // Link,
+} from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
+import Quote from './components/Quote';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div>
-      <Calculator />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
